654221ca735f7f26a235031b7251b10e2e0301ef
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2020-04-12 13:41:08 +0530

    Changes Merged.

--- src/app/sample-collection/waiting-list/waiting-list.component.ts
+++ src/app/sample-collection/waiting-list/waiting-list.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-waiting-list',
   templateUrl: './waiting-list.component.html',
-  styleUrls: ['./waiting-list.component.css'],
+  styleUrls: ['./waiting-list.component.scss'],
 })
 export class WaitingListComponent implements OnInit {
-
-  constructor() {
+  constructor(private readonly _router: Router) {
   }
 
   ngOnInit(): void {
   }
 
+  public async onUpdateSampleButtonClicked(): Promise<void> {
+    await this._router.navigate(['sample-collection/update-sample']);
+  }
 }
