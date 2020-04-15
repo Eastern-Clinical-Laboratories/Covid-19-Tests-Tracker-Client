3de50668d5d76eb17bc12d63a18a224394a44d38
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2020-04-15 21:57:49 +0530

    Changes Merged

---
+++ src/app/control-panel/organization/list-organization/list-organization.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-organization',
+  templateUrl: './list-organization.component.html',
+  styleUrls: ['./list-organization.component.css'],
+})
+export class ListOrganizationComponent implements OnInit {
+  public organizations: any[];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
